fb690cb8c1db1ff6a3ff51c95df9b77c309ee0cc
Ubuntu <azureuser@Ubuntu20.krc4m0pi3hpejkpikxfbpmfjah.bx.internal.cloudapp.net>
2023-02-13 02:30:03 +0000

    basic go application

---
+++ pasShield-enclave/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting:", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Println("Connected to localhost:8080")
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		fmt.Fprintf(conn, text+"\n")
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
