0588d352cebf921ed525b7b713f5668798e4e2ef
Brian Zhang <ericzhang64@yahoo.com>
2023-02-14 23:38:40 -0500

    update

--- pasShield-firefox/src/background.js
+++ pasShield-firefox/src/background.js
@@ -1,2 +1,27 @@
 // src/background.js
 const { AttestationClient } = require("@azure/attestation");
+
+
+
+
+
+function comingHeaders(details){
+    if( details.responseHeaders === undefined ) {
+        console.warn( "pasShield: no headers in the response" );
+        //console.log(details);
+        return;
+    }
+
+    details.responseHeaders.foreach(function());
+
+}
+
+
+
+//
+firefox.webRequest.onHeadersReceived.addListener(
+    comingHeaders,
+    {urls: ["<all_urls>"]},
+    ["responseHeaders", "blocking"]
+);
+
