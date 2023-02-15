c9c693016fe115e7af80ec831a3c55fb139533df
Tianchen Lan <ltc1143817203@gmail.com>
2023-02-15 02:12:49 +0000

    udate

--- pasShield-enclave/main.go
+++ pasShield-enclave/main.go
@@ -1,48 +1,137 @@
 package main
 
 import (
-    "bufio"
     "fmt"
     "net"
-    "strings"
+    "os"
+    "crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"math/big"
+	"net/http"
+	"time"
+
+	"github.com/edgelesssys/ego/enclave"
+)
+
+const (
+    CONN_HOST = "localhost"
+    CONN_PORT = "8080"
+    CONN_TYPE = "tcp"
 )
 
 func main() {
-    // 开启服务端
-    listener, err := net.Listen("tcp", "localhost:8080")
+
+    // Create certificate and a report that includes the certificate's hash.
+	cert, priv := createCertificate()
+	hash := sha256.Sum256(cert)
+	report, err := enclave.GetRemoteReport(hash[:])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+    tlsCfg := tls.Config{
+		Certificates: []tls.Certificate{
+			{
+				Certificate: [][]byte{cert},
+				PrivateKey:  priv,
+			},
+		},
+	}
+
+    // Create TCP server.
+    // Listen for incoming connections.
+    l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
     if err != nil {
         fmt.Println("Error listening:", err.Error())
-        return
+        os.Exit(1)
     }
-    defer listener.Close()
-    fmt.Println("Listening on localhost:8080")
 
+    
+
+
+    listener, err := tls.Listen("tcp", "0.0.0.0:8080", tlsCfg)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    defer listener.Close()
+    fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
     for {
-        // 等待客户端连接
-        conn, err := listener.Accept()
+        // Listen for an incoming connection.
+        conn, err := l.Accept()
         if err != nil {
             fmt.Println("Error accepting: ", err.Error())
-            return
+            os.Exit(1)
         }
+        // Handle connections in a new goroutine.
         go handleRequest(conn)
     }
-}
+}   
 
+// Handles incoming requests.
 func handleRequest(conn net.Conn) {
-    defer conn.Close()
-    // 获取客户端请求
-    request, err := bufio.NewReader(conn).ReadString('\n')
+    // Make a buffer to hold incoming data.
+    buf := make([]byte, 1024)
+    // Read the incoming connection into the buffer.
+    reqLen, err := conn.Read(buf)
+    fmt.Println(reqLen)
     if err != nil {
         fmt.Println("Error reading:", err.Error())
     }
-    request = strings.TrimSpace(request)
-	
-    // 处理请求并返回响应
-    response := processRequest(request)
-    conn.Write([]byte(response + "\n"))
+    // Send a response back to person contacting us.
+    conn.Write([]byte("Message received."))
+    // Close the connection when you're done with it.
+    conn.Close()
 }
 
-func processRequest(request string) string {
-    // 在这里处理请求并返回响应
-    return "Hello, " + request
+
+func createCertificate() ([]byte, crypto.PrivateKey) {
+	template := &x509.Certificate{
+		SerialNumber: &big.Int{},
+		Subject:      pkix.Name{CommonName: "localhost"},
+		NotAfter:     time.Now().Add(time.Hour),
+		DNSNames:     []string{"localhost"},
+	}
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	cert, _ := x509.CreateCertificate(rand.Reader, template, template, &priv.PublicKey, priv)
+	return cert, priv
+}
+
+func main() {
+	// Create certificate and a report that includes the certificate's hash.
+	cert, priv := createCertificate()
+	hash := sha256.Sum256(cert)
+	report, err := enclave.GetRemoteReport(hash[:])
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Create HTTPS server.
+
+	http.HandleFunc("/cert", func(w http.ResponseWriter, r *http.Request) { w.Write(cert) })
+	http.HandleFunc("/report", func(w http.ResponseWriter, r *http.Request) { w.Write(report) })
+	http.HandleFunc("/secret", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%v sent secret %v\n", r.RemoteAddr, r.URL.Query()["s"])
+	})
+
+	tlsCfg := tls.Config{
+		Certificates: []tls.Certificate{
+			{
+				Certificate: [][]byte{cert},
+				PrivateKey:  priv,
+			},
+		},
+	}
+
+	server := http.Server{Addr: "0.0.0.0:8080", TLSConfig: &tlsCfg}
+
+	fmt.Println("listening ...")
+	err = server.ListenAndServeTLS("", "")
+	fmt.Println(err)
 }
--- pasShield-server/flask_server.py
+++ pasShield-server/flask_server.py
@@ -1,20 +1,26 @@
 from flask import Flask, request,render_template
 import socket
 
-
 app = Flask(__name__)
+
+def init():
+    response_state = False
+    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client.connect(("localhost", 8080))
+    
+    tcp_client.sendall(b"Hello, server!")
+    
+    response = tcp_client.recv(1024)
+    print("Server says:", response.decode())
+    response_state = True
+
+    return 
 
 
 @app.route("/")
 def index():
-    tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp_client.connect(("localhost", 8080))
-    tcp_client.send("hello".encode("utf-8"))
-
-    # 等待 TCP 客户端响应并返回结果
-    #response = tcp_client.recv(1024).decode("utf-8")
     return render_template("index.html")
-    
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -23,4 +29,5 @@ def login():
     return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 if __name__ == "__main__":
+    init()
     app.run(host="0.0.0.0", port=5000)
