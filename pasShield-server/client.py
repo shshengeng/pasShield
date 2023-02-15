fe36970948e4f6a57126a102a86f176789aa2c68
rinkolite <62680624+rinkolite@users.noreply.github.com>
2023-02-14 21:39:14 -0500

    Add files via upload

---
+++ pasShield-server/client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'a socket example which send echo message to server.'
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 8080))
+print (s.recv(1024))
+for data in ['Michael', 'Tracy', 'Sarah']:
+    s.send(data.encode("utf-8"))
+    print (s.recv(1024))
+    print("start")
+s.send('exit'.encode("utf-8"))
+s.close()
