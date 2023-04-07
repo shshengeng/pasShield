632bd5ed4fd89391c412388cc71747573a99bc56
rinkolite <62680624+rinkolite@users.noreply.github.com>
2023-04-07 02:12:40 -0400

    Update app.py

--- python-Server/backend/app.py
+++ python-Server/backend/app.py
@@ -1,20 +1,3 @@
-from flask import Blueprint, render_template, session
-from flask_login import LoginManager, login_required, current_user
-
-from models import db, Users
-
-home = Blueprint('home', __name__, template_folder='../frontend')
-login_manager = LoginManager()
-login_manager.init_app(home)
-
-@home.route('/home', methods=['GET'])
-#@login_required
-def show():
-    if 'username' in session:
-        return render_template('home.html'), 200, [("Ego-Enclave-Attestation", "true")]
-    else:
-        return redirect(url_for('login.show')), 200, [("Ego-Enclave-Attestation", "true")]
-azureuser@Ubuntu20:/var/www/Flask-Login-Register/backend$ cat app.py 
 from flask import Flask
 import sqlalchemy
 from flask_login import LoginManager
